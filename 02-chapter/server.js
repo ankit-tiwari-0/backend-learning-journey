be495321d61821efe6dfc81a7f98362967daf210
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-04-24 19:41:08 +0530

    In ch-2, Creating Notes APIs & testing with Postman

---
+++ 02-chapter/server.js
@@ -0,0 +1,8 @@
+// main work of this folder is to start server 
+
+const app = require("./src/app")
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 300");
+    
+})
---
+++ 02-chapter/src/app.js
@@ -0,0 +1,7 @@
+//main work of this folder is to create server
+const express = require("express")
+
+
+const app = express()
+
+module.exports = app
