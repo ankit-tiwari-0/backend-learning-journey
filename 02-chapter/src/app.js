902063047e816483b64604f37b15f2bcf8ccf30c
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-04-25 13:33:09 +0530

    post api created

--- 02-chapter/src/app.js
+++ 02-chapter/src/app.js
@@ -3,5 +3,19 @@ const express = require("express")
 
 
 const app = express()
+app.use(express.json())
+
+const notes = []
+
+// crete notes API, title,  description
+// By useing of POST method /notes
+
+app.post('/notes', (req, res) =>{
+    notes.push(req.body);
+    res.status(201).json({
+        message: "note create successfully"
+    })
+    
+})
 
 module.exports = app
