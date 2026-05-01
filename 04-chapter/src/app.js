30b20cca205cb0f7a03dd1ebeb351ece2f4ba5b4
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-01 16:48:06 +0530

    delet api deleted data from db

--- 04-chapter/src/app.js
+++ 04-chapter/src/app.js
@@ -34,5 +34,17 @@ app.get("/notes",async (req, res) =>{
    })
 } )
 
+app.delete("/notes/:id", async (req, res) =>{
+    const id = req.params.id
+
+    await noteModel.findOneAndDelete({
+        _id: id
+    })
+
+    res.status(200).json({
+        message: "note Deleted successfully"
+    })
+})
+
   
 module.exports = app
