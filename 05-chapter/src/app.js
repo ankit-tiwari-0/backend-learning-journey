b62fc27d81f06f12b75b9263d4a6602b9c68badc
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-06 13:40:47 +0530

    backend is completed

--- 05-chapter/src/app.js
+++ 05-chapter/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const uploadFile = require('./services/storage.server');
+const postModel = require("./models/post.model")
 
 const app = express();
 app.use(express.json());
@@ -9,14 +10,30 @@ const upload = multer({storage:multer.memoryStorage()});
 
 
 app.post('/create-post', upload.single("image") ,async (req, res) =>{
-    console.log(req.body);
-    console.log(req.file.buffer);
 
     const result = await uploadFile(req.file.buffer)
-     console.log(result);
+     
+    const post = await postModel.create({
+        image: result.url,
+        caption: req.body.caption
+    })
+
+    return res.status(201).json({
+        message: "Post created",
+        post
+    })
      
     
     
+})
+
+app.get("/posts", async (req, res) =>{
+    const posts = await postModel.find()
+
+    return res.status(200).json({
+        message: "Posts fetch succe",
+        posts
+    })
 })
   
 module.exports = app;
