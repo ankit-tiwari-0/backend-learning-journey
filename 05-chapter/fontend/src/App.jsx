b9b3f0e1ee6d90615e9686864ad77243f64838ad
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-08 21:28:55 +0530

    Routes

--- 05-chapter/fontend/src/App.jsx
+++ 05-chapter/fontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      
+       <Routes>
+      <Route path='/' element={<h1>hello world</h1>} />
+      <Route path='/about' element={<h1>hello about</h1>} />
+    </Routes>
     </div>
   )
 }
