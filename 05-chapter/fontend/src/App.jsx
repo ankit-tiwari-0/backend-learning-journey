8d095fedf32d70a4ec49bff4b5626d7dbcb5752d
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-09 11:10:32 +0530

    Add createPost

--- 05-chapter/fontend/src/App.jsx
+++ 05-chapter/fontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import CreatePost from './pages/CreatePost'
 
 const App = () => {
   return (
     <div>
        <Routes>
-      <Route path='/' element={<h1>hello world</h1>} />
+      <Route path='/create-post' element={<CreatePost />} />
       <Route path='/about' element={<h1>hello about</h1>} />
     </Routes>
     </div>
