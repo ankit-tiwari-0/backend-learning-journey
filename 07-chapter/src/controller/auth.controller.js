9aa2ebea7e5a3b2ea7b14277259ac9dd2b92d4bd
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-05-15 22:06:13 +0530

    git add this

--- 07-chapter/src/controller/auth.controller.js
+++ 07-chapter/src/controller/auth.controller.js
@@ -6,6 +6,16 @@ async function registerUser(req, res) {
 
     const { username, email, password} = req.body;
 
+    const isUserExist = await userModel.findOne({
+        email
+    })
+
+    if(isUserExist){
+        return res.status(409)({
+            message: "User already exists"
+        })
+    }
+
     const user = await userModel.create({
         username, email, password
     })
