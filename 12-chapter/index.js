416227fe144a873dd22b54470087bdb2b4f99259
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-07-04 22:20:10 +0530

    here data shared in chunk

--- 12-chapter/index.js
+++ 12-chapter/index.js
@@ -1,9 +1,26 @@
 const http = require("http")
-
+const fs = require("fs")
 
 const server = http.createServer((req, res)=>{
-      res.end("Hello server is up")
+
+
+    //1 (●'◡'●) downloading file in a bad way  ❌
+   // const file = fs.readFileSync("sample.txt", "utf-8")
+   //    res.end(file)
+
+
+   //*2 Downloading file in a good way
+   
+   const readableStream = fs.createReadStream("sample.txt")
+   readableStream.pipe(res)    //method
+
+
+
+      
 })
+
+             
+
 
 
 server.listen(5000, ()=>{
