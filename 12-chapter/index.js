69c7ae011951c81111b8aca3fcc3171f16e4eadb
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-07-04 23:18:44 +0530

    String process

--- 12-chapter/index.js
+++ 12-chapter/index.js
@@ -5,38 +5,53 @@ const server = http.createServer((req, res)=>{
 
 
     //1 (●'◡'●) downloading file in a bad way  ❌
-   const file = fs.readFileSync("sample.txt", "utf-8")
-      res.end(file)
+   // const file = fs.readFileSync("sample.txt", "utf-8")
+   //    res.end(file)
 
 
    //*2 Downloading file in a good way
    
-   const readableStream = fs.createReadStream("sample.txt")
-   readableStream.pipe(res)    //method
-   res.end()
+//    const readableStream = fs.createReadStream("sample.txt")
+//    readableStream.pipe(res)    //method
+//    res.end()
 
 
 // -----------2------------
 
 // 1 copy file in bad way ❌
 
-const file = fs.readFileSync("sample.txt")
-fs.writeFileSync("output.txt", file)
-res.end
+// const file = fs.readFileSync("sample.txt")
+// fs.writeFileSync("output.txt", file)
+// res.end
 
 
 //2  copy file in good way
 
+// const readstream = fs.createReadStream("sample.txt");
+// const writestream = fs.createWriteStream("output.txt");
+
+// readstream.on("data", (chank)=>{
+//     console.log("CHUNK :", chank);
+//     writestream.write(chank)
+    
+// })
+
+
+/// ? -----------------String process------------------
+// upparcasw
+// ipsum ----> ankit
+
+
 const readstream = fs.createReadStream("sample.txt");
 const writestream = fs.createWriteStream("output.txt");
 
-readstream.on("data", (chank)=>{
-    console.log("CHUNK :", chank);
-    writestream.write(chank)
-    
+
+readstream.on("data", (chunk) =>{
+    const modify = chunk.toString().toUpperCase().replaceAll(/ipsum/gi, "ankit");
+    writestream.write(modify)
 })
 
-
+res.end()
 
 
       
