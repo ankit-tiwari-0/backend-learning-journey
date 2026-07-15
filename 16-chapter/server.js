5cd7b6df6bdadff4c227b6e1cbb834bb6d0fee3b
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-07-15 13:02:25 +0530

    16-chapter completed

--- 16-chapter/server.js
+++ 16-chapter/server.js
@@ -1,18 +1,42 @@
 import express from "express"
+import cookieParser from "cookie-parser"
 
 
 const app = express()
+
+app.use(cookieParser("secter"))
 
 
 
 
 app.get('/', (req, res)=>{
    
-
+// //! how cookies set
     res.cookie("name", "express" ,{
-        maxAge: 1000 * 60 * 60
+        maxAge: 1000 * 60 * 60,
+        signed: true
+
     })
     res.send('helooo')
+    
+})
+
+
+app.get('/product', (req, res)=>{
+
+    console.log('cookies:', req.cookies);
+    console.log('singcookies', req.signedCookies);
+    
+
+    if (req.cookies.name && req.cookies.name === "express") {
+      res.status(200).send({
+        id: 1,
+        "name": "Item",
+        "price": "$100"
+    })  
+    }
+    res.status(404).send("not allow ")
+    
     
 })
 
@@ -22,3 +46,6 @@ app.listen(3000, ()=>{
     console.log("server is running on 3000");
     
 })
+
+
+//&& ?? operator
