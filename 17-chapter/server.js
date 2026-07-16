cc28902441d7ab08e2f9a101c207b220bbd4aba7
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-07-16 19:43:24 +0530

    session

--- 17-chapter/server.js
+++ 17-chapter/server.js
@@ -1,11 +1,29 @@
 import express from 'express'
+import session from 'express-session'
+import cookieParser from 'cookie-parser'
 
 
 const app = express()
 
+app.use(session(
+    {
+        secret: "ankiit",
+        saveUninitialized: false,
+        resave: false,
+        cookie:{
+            maxAge: 1000*60*60
+        }
+    }
+))
+app.use(cookieParser("ankkkit"))
+
 app.get("/", (req, res)=>{
+    console.log(req.session);
+    console.log(req.session.id);
+    
     res.status(200).send("helooo")
 })
+
 
 
 app.listen(3000, ()=>{
