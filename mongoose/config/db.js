a936c007f5e16c5f262ff92afab4c47ae46edddd
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-07-23 12:26:45 +0530

    connected to db

--- mongoose/config/db.js
+++ mongoose/config/db.js
@@ -1,3 +1,16 @@
 import mongoose from 'mongoose'
 
 
+const connectDB = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGOOSE)
+        console.log("CHAL RHA HAI ");
+        
+    } catch (error) {
+        console.log("ERROR" ,  error.message);
+        process.exit(1)
+    }
+}
+
+
+export default connectDB
--- mongoose/server.js
+++ mongoose/server.js
@@ -1,6 +1,8 @@
  import express from 'express'
+ import dotenv from "dotenv"
+ import connectDB from './config/db.js'
 
-
+dotenv.config()
  const app = express()
 
 
@@ -12,6 +14,7 @@ app.get("/", (req, res)=>{
 
 
 
+connectDB();
 
  app.listen(3000, ()=>{
     console.log("server is running 300");
