b87cb072dc9c44c84abf8af055699d14a3a45b2c
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-07-27 22:19:37 +0530

    task.modelSchema

---
+++ Authentication/model/task.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+    userID: {type:mongoose.Schema.Types.ObjectId, ref:"UserModel", required:true},
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type:String
+    }
+}, {timestamps: true})
+
+const TASK = mongoose.model("Task", taskSchema)
+
+1032
