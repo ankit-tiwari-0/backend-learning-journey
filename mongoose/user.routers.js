9fc3aab66ff4f05451ebb0baf223e896a73ed6f6
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-07-27 12:26:50 +0530

    add delete, put, get api

--- mongoose/user.routers.js
+++ mongoose/user.routers.js
@@ -25,20 +25,77 @@ router.post("/user", async (req , res)=>{
 })
 
 
-router.get("/user", (req , res)=>{
-   
+router.get("/user", async (req , res)=>{
+   try {
+    const Users = await UserModel.find()
+
+    res.status(200).json({
+        success: true,
+        data: Users,
+        message: "done"
+    })
+   } catch (error) {
+    console.log(error);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    })
+   }
 
 })
 
 
-router.put("/user", (req , res)=>{
-   
+router.put("/user/:id", async (req , res)=>{
+    const {id} = req.params;
+    const {name, age, weight} = req.body;
+    try {
+        const updateuser = await UserModel.findByIdAndUpdate(id, {name, age, weight}, {returnDocument:true , runValidators:true});
+
+        if(!updateuser){
+            return res.status(401).json({
+                success:false,
+                message: "usern not "
+            })
+        }
+
+        res.json({
+            success:true,
+            user: updateuser
+        })
+    } catch (error) {
+        console.log(error);
+    res.status(500).json({
+        success: false,
+        message: error.message
+    })
+    }
 
 })
 
 
-router.delete("/user", (req , res)=>{
-   
+router.delete("/user/:id", async (req , res)=>{
+   try {
+    const {id} = req.params;
+    const delett = await UserModel.findByIdAndDelete(id);
+     if (!delett) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+      data: delett,
+    });
+   } catch (error) {
+    res.status(500).json({
+        sucess: false,
+        messages: "deleted",
+        message:error.message
+    })
+   }
 
 })
 
