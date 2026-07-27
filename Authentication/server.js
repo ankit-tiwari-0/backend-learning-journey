f2c7877aef765c72cbe60291a7c6b46c9b744897
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-07-27 21:48:53 +0530

    express-session

--- Authentication/server.js
+++ Authentication/server.js
@@ -1,10 +1,23 @@
  import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './db/db.js'
+import session from 'express-session'
 
+dotenv.config()
  const app = express()
+ app.use(express.json())
+
+ app.use(
+    session({
+        secret: process.env.SECRET_KEY,
+        resave:false,
+        saveUninitialized:true,
+        cookie:{maxAge:600000}
+    })
+ )
 
 
-
+connectDB()
  app.listen(3000, ()=>{
     console.log("server is running");
     
