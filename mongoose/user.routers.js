a57066ddc4c683937c07f4a0d38193b11e4ae7df
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-07-27 11:37:05 +0530

    add api /user

--- mongoose/user.routers.js
+++ mongoose/user.routers.js
@@ -4,7 +4,23 @@ import UserModel from "./model/user.model.js";
 const router = Router();
 
 router.post("/user", async (req , res)=>{
-   
+   try {
+    const {name , age , weight} = req.body;
+
+   const newuser = new UserModel({name, age, weight})
+   await newuser.save();
+
+   res.status(201).json({
+       sucess: true,
+       data: newuser
+   })
+   } catch (error) {
+    console.log(error);
+    res.status(500).json({ 
+        success:false,
+        message:error.message 
+    })
+   }
 
 })
 
