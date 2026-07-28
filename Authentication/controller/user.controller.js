e19013ddf41f88a9fc9f5231ddd977433908c66e
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-07-28 22:04:15 +0530

    signup

--- Authentication/controller/user.controller.js
+++ Authentication/controller/user.controller.js
@@ -1,5 +1,22 @@
-export const signup = ()=>{
-    
+
+
+export const signup = async ()=>{
+    const {username, password} = req.body;
+
+    try {
+        const user = await registerUser(username, password);
+        res.status(201).json({
+            success: true,
+            messsage: "user register successfull",
+            data:user
+        })
+    } catch (error) {
+     res.status(500).json({
+        success: false,
+        messsage: "errorr",
+        error: error.messsage
+     })   
+    }
 }
 
 export const login = ()=>{
