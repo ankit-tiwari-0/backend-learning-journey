b02dd9db4f404b4db86bd7f95bbb96c03349ff85
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-03 18:32:22 +0530

    how to use ws

---
+++ socket/index.js
@@ -0,0 +1,29 @@
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+
+const server = http.createServer((req, res) => {
+    console.log(new Date() + " Received req for " + req.url);
+    res.end("Hi there");
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+    ws.on("error", console.error);
+
+    ws.on("message", function message(data, isBinary) {
+
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+
+    });
+
+    ws.send("Hello! connection message form ws server");
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 8080");
+});
