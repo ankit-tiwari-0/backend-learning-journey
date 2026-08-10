89fe75e3eb000a9a747f7d17e46ba2a0609bab7c
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-10 11:49:27 +0530

    Setting up the Editor

--- 26 -Docker+AWS/Frontend/src/App.jsx
+++ 26 -Docker+AWS/Frontend/src/App.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main
+      className='h-screen w-full bg-gray-800 flex gap-4 p-4'>
+
+      <aside
+        className='h-full w-1/4 bg-amber-50 rounded-lg'>
+      </aside>
+       <section 
+       className='h-full w-3/4 bg-neutral-800 rounded-lg'>
+
+       </section>
+    </main>
   )
 }
 
