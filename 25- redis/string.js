c333729d1a99f959abf826d05c3c4e18fe10ec28
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-13 16:19:04 +0530

    set

--- 25- redis/string.js
+++ 25- redis/string.js
@@ -2,7 +2,11 @@ const client = require("./client")
 
 async function init() {
   const   data = await client.get("name:2")
+  await client.set("msg:6", "hey from nodejs")
+  const msg = await client.get("msg:6")
   console.log(data);
+  console.log(msg);
+  
   
 }
 
