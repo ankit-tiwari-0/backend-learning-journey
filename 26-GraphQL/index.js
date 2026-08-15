fbbf692b0f39860460f4a7ce6ecd5f1d339623b1
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-15 20:41:00 +0530

    typeDefs, resolvers

--- 26-GraphQL/index.js
+++ 26-GraphQL/index.js
@@ -1,15 +1,35 @@
 import express from "express"
 import { ApolloServer } from "@apollo/server"
-import { expressMiddleware } from "@apollo/server/express5"
+import { expressMiddleware } from "@as-integrations/express5"
 import bodyParser from "body-parser"
 import cors from "cors"
 
 
 async function startServer(params) {
     const app = express()
+   
+    const typeDefs = `
+    type Query {
+       hello:String
+       name:String
+    }
+    `
+    const resolvers = {
+        Query:{
+            hello:()=>"hello world!",
+            name:()=>"ankit"
+        }
+    }
 
+    const server = new ApolloServer({typeDefs, resolvers})
 
+    await server.start()
+    app.use(bodyParser.json());
+    app.use(cors());
+    app.use("/graphql", expressMiddleware(server))  
     app.listen(8000, ()=>{
-        console.log("Server is running")
+        console.log("Server is runnig")
     })
 }
+
+startServer()
